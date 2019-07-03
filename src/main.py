8a0b6917f4c61f656576b812293aa25682f4a3cd
123 <123>
2019-07-03 18:48:26 +0400

    Some work on frontend. Fixed new_board method.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from board_module import Board
 from entity_module import *
 from log_module import print_to_log, get_log, clear_log
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='data', static_url_path='/data')
 
 board = Board(1, 1, 0)
 
@@ -15,30 +15,34 @@ def new_board():
     global board
 
     try:
-        n, m = 5, 5
+        # print(request.form.get(cols))
+        m, n = int(request.form.get('cols')), int(request.form.get('rows'))
+        animals = int(request.form.get('animals'))
 
-        board = Board(n, m, 3)
+        board = Board(m, n, 3)
 
-        for i in range(3):
+        for i in range(animals):
             board.board[random.randint(0, n - 1)][random.randint(0, m - 1)].append(
-                GrassFeeding(random.randint(0, 20), str(i + 4)))
-        for i in range(3):
+                GrassFeeding(random.randint(0, 20), str(i + animals + 1)))
+        for i in range(animals):
             board.board[random.randint(0, n - 1)][random.randint(0, m - 1)].append(
                 SmallPredator(random.randint(50, 200), str(i + 1)))
-        for i in range(2):
+        for i in range(animals//2+1):
             board.board[random.randint(0, n - 1)][random.randint(0, m - 1)].append(
-                BigPredator(random.randint(200, 300), str(i + 7)))
-        for i in range(5):
+                BigPredator(random.randint(200, 300), str(i + animals * 2 + 1)))
+        for i in range(animals*2):
             board.board[random.randint(0, n - 1)][random.randint(0, m - 1)].append(Plant(random.randint(0, 40), 'kust'))
         board.board[random.randint(0, n - 1)][random.randint(0, m - 1)].append(Watcher('Man'))
 
         clear_log()
 
         return 'OK.'
-    except:
+    except Exception as exep:
+        print(exep)
         return 'Error!'
 
 
+@app.route('/', methods=['GET'])
 @app.route('/board/', methods=['GET'])
 def get_board_now():
     return render_template('index.html')
